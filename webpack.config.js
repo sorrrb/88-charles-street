586307ff7787af835861901b8f7dc6bb62a4b862
sorrrb <cdrukepike@gmail.com>
2024-02-04 18:33:21 -0500

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },      {
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
